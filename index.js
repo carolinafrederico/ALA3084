22669c9818d4269f925606e3541e1d716299ddbf
carolinafrederico <iam@carolinafrederico.com>
2025-03-04 14:20:06 -0500

    Part 1. Exerc1.1

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // PART 1  – Loop through all numbers from 1 to 100.
-let n = 0;
-while (n <= 100) {
-    console.log(n);{
-        n++
-    }
+// let n = 0;
+// while (n <= 100) {
+//     console.log(n);{
+//         n++
+//     }
+// }
+for (let n = 0; n <= 100; n++){
+    console.log(n);
 }
