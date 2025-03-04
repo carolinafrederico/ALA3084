8d291c978fd1f275a535dd285f19b7f2ac20fe12
carolinafrederico <iam@carolinafrederico.com>
2025-03-04 11:27:42 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let movies = ['Shrek', 'Caddyshack', 'Batman', 'Interstellar', 'Scarface', 'Spaceballs', 'Alien', 'The Count of Monte Cristo'];
+
+for (let movie of movies) {
+	if (movie === 'The Last Airbender') break;
+	console.log(movie);
+}
