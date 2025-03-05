7934994b2329ed5874776bb0044f1cdd936df98c
carolinafrederico <iam@carolinafrederico.com>
2025-03-04 22:11:38 -0500

    Expand Funcionality

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-// PART 1  – Loop through all numbers from 1 to 100.
+// PART 1  – REFACTORING OLD CODE 
+// Loop through all numbers from 1 to 100.
 // let n = 0;
 // while (n <= 100) {
 //     console.log(n);{
 //         n++
 //     }
 // }
-for (let n = 0; n <= 100; n++){
-    console.log(n);
-}
+// for (let n = 0; n <= 100; n++){
+//     console.log(n);
+// }
+// EXPANDING FUNCIONALITY
+
+let array = ""
+
+const inputStr = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+  const strRows = inputStr.split("\n");
+  for (let i = 0; i <= strRows.length-1; i++) {
+	const cell = strRows[i].split(",");
+	console.log(cell);
+  }
